15f82d9301eb13cef4cac9b50969f0f25107ba29
rohan <rohanneps@gmail.com>
2017-03-14 11:53:17 +0545

    Initial commit

---
+++ Scrapper Test/gbd/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+ 
+setup(
+    name = 'gbd',
+    packages = ['gbd'],
+    version = '0.0.1',
+    description = 'Test module',
+    author='Rohan Amatya',
+    author_email='rohanneps@gmail.com',
+)
