b791450fb10cfafdd4eefa03ccb1bb2d4965b630
rohanneps <rohanneps@gmail.com>
2017-03-14 15:45:51 +0545

    New module test

--- Num_Test/setup.py
+++ Num_Test/setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'gbd_de',
     packages = ['Num_Test'],
     version = '0.0.1',
-    url='https://github.com/rohanneps/rohanneps/tree/master/Scrapper_Test/gbd',
+    url='https://github.com/rohanneps/rohanneps/tree/master/Num_Test',
     description = 'Get even or odd',
     author='Rohan Amatya',
     keywords='Even Odd',
