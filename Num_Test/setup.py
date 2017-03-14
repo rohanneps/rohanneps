b1e892c1c6356ccc73fa2c9d83f754b920c62dcf
rohanneps <rohanneps@gmail.com>
2017-03-14 14:04:47 +0545

    New module test

---
+++ Num_Test/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+ 
+setup(
+    name = 'gbd_de',
+    packages = ['Num_Test'],
+    version = '0.0.1',
+    url='https://github.com/rohanneps/rohanneps/tree/master/Scrapper_Test/gbd',
+    description = 'Get even or odd',
+    author='Rohan Amatya',
+    keywords='Even Odd',
+    author_email='rohanneps@gmail.com',
+)
